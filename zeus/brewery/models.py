d729c4ddd8a7bee790bd31833ba28a6f90fd81a7
Divakar Rajesh <sdivakarrajesh@gmail.com>
2020-03-25 22:06:22 +0530

    Brewery: models and migrations

--- zeus/brewery/models.py
+++ zeus/brewery/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class DrinkType(models.Model):
+
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    title = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.title or ''
+
+class Drink(models.Model):
+
+    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    name = models.CharField(max_length=300, null=True, blank=True)
+    image = models.URLField(blank=True, null=True)
+    drink_type = models.ManyToManyField(DrinkType, blank=True)
+
+    def __str__(self):
+        return self.name or ''
